e8603d9daf88a0eda69bca6672acb65b0c995dbf
mieco7832 <65424361+mieco7832@users.noreply.github.com>
2021-02-19 14:26:53 -0600

    Update Conector.php

--- echo32_facturas-php-1.0/class/config/Conector.php
+++ echo32_facturas-php-1.0/class/config/Conector.php
@@ -3,7 +3,7 @@
 class conector {
 
     public static function conectar() {
-        $db = new PDO("mysql:host=localhost;dbname=factura_32", "root", "root");
+        $db = new PDO("mysql:host=localhost;dbname=factura_32", "****", "****");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     }
